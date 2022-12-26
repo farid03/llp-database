1063f33fcd21047714063521d5c3d8b90eb93641
Farid Kurbanov <F.Kurbanov120303@yandex.ru>
2022-12-26 20:52:39 +0300

    added functions to node serialization, some refactor

--- dbstruct/serialization.h
+++ dbstruct/serialization.h
@@ -9,10 +9,12 @@
 #include "../file_workers/file_utils.h"
 
 std::unordered_map<std::string, data_type> deserialize_file_header_schema(int32_t fd, int64_t header_size) {
-    auto buff = malloc(header_size - offsetof(tree_header, schema));
+    auto size = header_size - offsetof(tree_header, schema);
+    auto buff = malloc(size);
     auto cfd = open_file(".cache");
-    read_from_file(fd, offsetof(tree_header, schema), buff, header_size - offsetof(tree_header, schema));
-    write_into_file(cfd, 0, buff, header_size - offsetof(tree_header, schema)); // зачем
+
+    read_from_file(fd, offsetof(tree_header, schema), buff, size);
+    write_into_file(cfd, 0, buff, size); // записываем десериалиуемый контейнер из бд в кэш
     std::ifstream ifs(".cache"); // десериализуем
     boost::archive::text_iarchive ia(ifs);
     std::unordered_map<std::string, data_type> map = {};
@@ -30,11 +32,43 @@ void serialize_and_cache_file_header(std::unordered_map<std::string, data_type> 
     ofs.close();
 }
 
-size_t get_cache_size(int32_t cached_fd) {
+size_t get_cache_size(const int32_t cached_fd) {
     struct stat64 cache_file_stat = {0};
     fstat64(cached_fd, &cache_file_stat);
 
     return cache_file_stat.st_size;
 }
 
+void move_from_cache_to_db(int32_t fd, int32_t cached_fd, int64_t file_offset) {
+    auto cfd_size = get_cache_size(cached_fd);
+    void *buff = malloc(cfd_size);
+    read_from_file(cached_fd, 0, buff, cfd_size);
+    write_into_file(fd, file_offset, buff, cfd_size); // записываем сериализованную схему в бд
+    free(buff);
+}
+
+std::unordered_map<std::string, std::string> deserialize_node_data(int32_t fd, int64_t node_offset, int64_t node_size) {
+    auto offset = node_offset + offsetof(node, data);
+    auto size = node_size - offsetof(node, data);
+    auto buff = malloc(size);
+    auto cfd = open_file(".cache");
+
+    read_from_file(fd, offset, buff, size);
+    write_into_file(cfd, offset, buff, size); // записываем десериалиуемый контейнер из бд в кэш
+    std::ifstream ifs(".cache"); // десериализуем
+    boost::archive::text_iarchive ia(ifs);
+    std::unordered_map<std::string, std::string> map = {};
+    ia >> map;
+    ifs.close();
+    free(buff);
+
+    return map;
+}
+
+void serialize_and_cache_node_data(std::unordered_map<std::string, std::string> &name_to_type) {
+    std::ofstream ofs(".cache"); // сериализуем
+    boost::archive::text_oarchive oa(ofs);
+    oa << name_to_type; // записываем сериализованные данные в кэш
+    ofs.close();
+}
 #endif //LLP_DATABASE_SERIALIZATION_H
--- dbstruct/tree_header.cpp
+++ dbstruct/tree_header.cpp
@@ -43,17 +43,13 @@ bool initialize_db(int32_t fd, std::unordered_map<std::string, data_type> &name_
 
     auto cfd = open_file(".cache"); // блок для сериализации
     serialize_and_cache_file_header(name_to_type);
-    auto cfd_size = get_cache_size(cfd);
-    void *buff = malloc(cfd_size);
-    read_from_file(cfd, 0, buff, cfd_size);
-    write_into_file(fd, offsetof(tree_header, schema), buff, cfd_size); // записываем сериализованную схему в бд
+    move_from_cache_to_db(fd, cfd, offsetof(tree_header, schema));
 
-    header.size = offsetof(tree_header, schema) + cfd_size - 1; // валидируем размер хедера файла с учетом сериализованной схемы
+    header.size = offsetof(tree_header, schema) + get_cache_size(cfd) - 1; // валидируем размер хедера файла с учетом сериализованной схемы
     auto first_node = read_node_header_from_db(fd, header.first_node);
     header.first_free_space = int64_t(first_node.offset + first_node.size + 1); // создаем искуственно первую пустую ноду
     set_tree_header_to_db(fd, header);
     write_free_space_to_db(fd, space, header.first_free_space); // возможно выйдет обойтись без этого приема, если обработать отсутствие пустой ноды в файле при вставках
-    free(buff);
 
     return true;
 }
