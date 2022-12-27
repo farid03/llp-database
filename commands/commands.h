d113847b8241ad6dda62f2e77bd1ecf91e176abe
Farid Kurbanov <F.Kurbanov120303@yandex.ru>
2022-12-28 00:53:05 +0300

    minor changes in description

--- commands/commands.h
+++ commands/commands.h
@@ -1,6 +1,8 @@
 #ifndef LLP_DATABASE_COMMANDS_H
 #define LLP_DATABASE_COMMANDS_H
 #include <cinttypes>
+#include <unordered_map>
+#include <string>
 
 enum command_type {
     ADD,
@@ -10,13 +12,12 @@ enum command_type {
     FIND_ALL,
     DELETE,
 };
-
+// TODO добавить описания
 // TODO подумать над структурой запроса
 struct command {
 
 };
 
-int32_t get_next_node_id(int32_t fd);
-int64_t add_node(int32_t fd, const std::unordered_map<std::string, std::string> &node_data, bool is_update);
+int64_t add_node(int32_t fd, int64_t parent_id, const std::unordered_map<std::string, std::string> &node_data);
 
 #endif //LLP_DATABASE_COMMANDS_H
--- data_process_utils/validation/schema_validation.h
+++ data_process_utils/validation/schema_validation.h
@@ -4,10 +4,11 @@
 #include <cstdint>
 #include <string>
 #include <unordered_map>
-
+// TODO добавить описания
 bool is_valid_int(const std::string &s);
 
 bool is_valid_float(const std::string &s);
-bool validate_schema(int32_t fd, const std::unordered_map<std::string, std::string> &node_data);
+
+bool validate_node_by_schema(int32_t fd, const std::unordered_map<std::string, std::string> &node_data);
 
 #endif //LLP_DATABASE_SCHEMA_VALIDATION_H
