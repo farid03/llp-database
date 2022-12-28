f65b54e81dc6ae8085fd36af6066187cba59e5b0
Farid Kurbanov <F.Kurbanov120303@yandex.ru>
2022-12-28 12:02:22 +0300

    function validate_field_by_schema is isolated from function validate_node_by_schema

--- data_process_utils/validation/schema_validation.cpp
+++ data_process_utils/validation/schema_validation.cpp
@@ -16,38 +16,46 @@ bool is_valid_float(const std::string &s) {
     }
     return s.find_first_not_of("0123456789.", offset) == std::string::npos;
 }
-//
-// true, в случае успеха, иначе false
+
+bool validate_field_by_schema(const std::unordered_map<std::string, data_type> &schema,
+                              const std::pair<const std::basic_string<char>, std::basic_string<char>> &a) {
+    if (schema.count(a.first) != 1) { // валидируем название полей ноды по схеме, названия уникальны (map)
+        return false;
+    }
+    switch (schema.at(a.first)) {
+        case BOOL:
+            if (a.second != "true" && a.second != "false") {
+                return false;
+            }
+            break;
+        case INT:
+            if (!is_valid_int(a.second)) {
+                return false;
+            }
+            break;
+        case FLOAT:
+            if (!is_valid_float(a.second)) {
+                return false;
+            }
+            break;
+        case STRING:
+            break;
+        default:
+            return false;
+    }
+
+    return true;
+}
+
 bool validate_node_by_schema(int32_t fd, const std::unordered_map<std::string, std::string> &node_data) {
     struct tree_header header = get_tree_header_from_db(fd);
     if (node_data.size() != header.schema.size()) { // валидиуем количество полей
         return false;
     }
 
-    for (const auto& a: node_data) {
-        if (header.schema.count(a.first) != 1) { // валидируем название полей ноды по схеме, названия уникальны (map)
+    for (const auto &a: node_data) {
+        if (!validate_field_by_schema(header.schema, a)) {
             return false;
-        }
-        switch (header.schema[a.first]) {
-            case BOOL:
-                if (a.second != "true" && a.second != "false") {
-                    return false;
-                }
-                break;
-            case INT:
-                if (!is_valid_int(a.second)) {
-                    return false;
-                }
-                break;
-            case FLOAT:
-                if (!is_valid_float(a.second)) {
-                    return false;
-                }
-                break;
-            case STRING:
-                break;
-            default:
-                return false;
         }
     }
 
--- data_process_utils/validation/schema_validation.h
+++ data_process_utils/validation/schema_validation.h
@@ -4,11 +4,16 @@
 #include <cstdint>
 #include <string>
 #include <unordered_map>
+#include "../../dbstruct/tree_header.h"
+
 // TODO добавить описания
 bool is_valid_int(const std::string &s);
 
 bool is_valid_float(const std::string &s);
 
 bool validate_node_by_schema(int32_t fd, const std::unordered_map<std::string, std::string> &node_data);
+
+bool validate_field_by_schema(const std::unordered_map<std::string, data_type> &schema,
+                              const std::pair<const std::basic_string<char>, std::basic_string<char>> &a);
 
 #endif //LLP_DATABASE_SCHEMA_VALIDATION_H
