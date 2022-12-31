7ca8cbeb4847ac31647d488c33521b1088ef7d8e
Farid Kurbanov <F.Kurbanov120303@yandex.ru>
2022-12-31 15:33:13 +0300

    added condition and query structs, changed public interface functions, fixed bug in function delete_node

--- commands/commands.cpp
+++ commands/commands.cpp
@@ -1,128 +1,125 @@
 #include <stack>
 #include "commands.h"
 #include "../data_process_utils/validation/schema_validation.h"
-#include "../dbstruct/node.h"
+#include "iterator/result_iterator.h"
 
 /** Добавляет новый документ в дерево.
  * @param fd - файловый дескриптор
  * @param node - новый узел, который нужно добавить | node = map[field_name]: data
  * @return возвращает id записанного узла, -1 в случае неудачи
  */
-int64_t add_node(const int32_t fd, int64_t parent_id, const std::unordered_map<std::string, std::string> &node_data) {
+int64_t add_node(const int32_t fd, int32_t parent_id, const std::unordered_map<std::string, std::string> &node_data) {
     if (!validate_node_by_schema(fd, node_data)) {
         printf("Invalid node_data. Field must be match the schema!\n");
         return -1;
     }
     struct node node = {0};
     node.id = get_next_node_id(fd);
-    node.parent_id = parent_id;
+    auto parent = read_node_header_from_db(fd, idx.id_to_offset[parent_id]);
+    node.parent = parent.offset;
     node.data = node_data;
     int64_t node_offset = 0;
     //  offset, size, r_size будут заданы дальше, при записи
 
-    if (!idx.parent_to_childs[parent_id].empty()) { // если у родителя есть уже другие дети
-        auto last_parent_child = idx.parent_to_childs[parent_id].end(); // добавим в лист родителя нового ребенка
-        node.prev = idx.id_to_offset[*(--last_parent_child)];
+    if (parent.first_child != 0) { // если у родителя есть уже другие дети
+        auto last_parent_child = read_node_header_from_db(fd, parent.first_child); // найдем последнего ребенка родителя
+        while (last_parent_child.next != 0) {
+            last_parent_child = read_node_header_from_db(fd, last_parent_child.next);
+        }
+        node.prev = last_parent_child.offset;
         node_offset = write_node_to_db(fd, node);
-        auto prev_node = read_node_from_db(fd, node.prev); // укажем предыдущей ноде родителя новую
-        prev_node.next = node_offset;
-        write_node_to_db(fd, prev_node, prev_node.offset);
+        last_parent_child.next = node_offset; // укажем предыдущей ноде родителя нового соседа
+        if (!write_node_header_to_db(fd, last_parent_child, last_parent_child.offset)) {
+            printf("Error in write_node_header_to_db! Rewriting last parent child node in function add not valid!\n");
+            return -1;
+        }
     } else {
         node_offset = write_node_to_db(fd, node);
-        auto parent_node = read_node_from_db(fd, idx.id_to_offset[parent_id]);
+        auto parent_node = read_node_header_from_db(fd, parent.offset);
         parent_node.first_child = node_offset;
-        if (!write_node_to_db(fd, parent_node, parent_node.offset)) {
-            printf("Error in write_node_to_db! Rewriting parent node in function add not valid!\n");
+        if (!write_node_header_to_db(fd, parent_node, parent_node.offset)) {
+            printf("Error in write_node_header_to_db! Rewriting parent node in function add not valid!\n");
             return -1;
         }
     }
 
-    add_node_to_index(node.id, parent_id, node_offset);
+    add_node_to_index(node.id, node_offset);
     return node.id;
 }
 
-std::unordered_map<std::string, std::string> find_node_by_id(const int32_t fd, int64_t id) {
+// TODO придется выпилить и оставить только нижни find
+std::unordered_map<std::string, std::string> find_node_by_id(const int32_t fd, int32_t id) {
     if (idx.id_to_offset.count(id) == 0) {
-        printf("Node with such an id{%ld} doesn't exist!\n", id);
+        printf("Node with such an id{%d} doesn't exist!\n", id);
         return {};
     }
     auto node = read_node_from_db(fd, idx.id_to_offset[id]);
     node.data["id"] = std::to_string(node.id);
-    node.data["parent_id"] = std::to_string(node.parent_id);
-
+    if (node.parent != 0) {
+        node.data["parent_id"] = std::to_string(read_node_header_from_db(fd, node.parent).id);
+    }
     return node.data;
 }
 
-std::list<std::unordered_map<std::string, std::string>> find_node_by_parent(const int32_t fd, int64_t parent_id) {
-    std::list<std::unordered_map<std::string, std::string>> result{};
-    if (idx.parent_to_childs.count(parent_id) == 0) {
-        printf("Node with such a parent_id doesn't exist!\n");
-        return {};
-    }
-
-    for (auto id: idx.parent_to_childs[parent_id]) {
-        result.emplace_back(find_node_by_id(fd, id));
-    }
-
-    return result;
-}
-
-// TODO узнать насколько разумен такой формат возврата результатов, не разумен
-std::list<std::unordered_map<std::string, std::string>> find_all(const int32_t fd) {
-    std::list<std::unordered_map<std::string, std::string>> result{};
-
-    for (auto node: idx.id_to_offset) {
-        result.emplace_back(find_node_by_id(fd, node.first));
-    }
-
-    return result;
-}
-
-// условие можно задавать только на поля содержащиеся в заданной схеме
-std::list<std::unordered_map<std::string, std::string>>
-find_node_by_condition(const int32_t fd, const std::pair<std::string, std::string> &condition) {
+struct result_iterator find(const int32_t fd, const struct condition &condition) {
     auto header = get_tree_header_from_db(fd);
-    if (!validate_field_by_schema(header.schema, condition)) {
-        printf("Invalid condition in function find_node_by_condition!\n");
+    if (!validate_field_by_schema(header.schema, {condition.name_value, condition.value})) {
+        printf("Invalid condition in function find!\n");
         return {};
     }
-    std::list<std::unordered_map<std::string, std::string>> result{};
-    for (auto node: idx.id_to_offset) {
-        auto current_node = read_node_from_db(fd, node.second);
-        if (current_node.data[condition.first] == condition.second) {
-            current_node.data["id"] = std::to_string(current_node.id);
-            current_node.data["parent_id"] = std::to_string(current_node.parent_id);
-            result.emplace_back(current_node.data);
-        }
-    }
+    struct result_iterator res = result_iterator(fd, condition);
 
-    return result;
+    return res;
 }
 
-bool delete_node(int32_t fd, int64_t id) {
+struct result_iterator find_all(int32_t fd) {
+    return {fd, condition("nop", "nop", NOP)};
+}
+
+
+bool delete_node(int32_t fd, int32_t id) {
     // при удалении не забыть освободить и выделить free_space на предыдущем месте
     if (id == 0) { // корневой узел нельзя удалять
         printf("Cannot delete a node with ID 0.");
         return false;
     }
     if (idx.id_to_offset.count(id) == 0) {
-        printf("Node with such an id{%ld} doesn't exist!\n", id);
+        printf("Node with such an id{%d} doesn't exist!\n", id);
         return false;
     }
 
     std::stack<int64_t> rm_nodes_offset = {};
     auto node_to_remove = read_node_header_from_db(fd, idx.id_to_offset[id]);
-    remove_node_from_index(node_to_remove.id, node_to_remove.parent_id);
+    remove_node_from_index(node_to_remove.id);
     remove_node_from_db(fd, node_to_remove);
     if (node_to_remove.first_child != 0) {
         rm_nodes_offset.push(node_to_remove.first_child);
     }
 
+    // нужно обновить ссылки у родителя и соседних детей на нас
+    auto parent = read_node_header_from_db(fd, node_to_remove.parent);
+    if (parent.first_child == node_to_remove.offset) { // если мы единственный ребенок (у родителя есть на нас указатель
+        parent.first_child = 0;
+        write_node_header_to_db(fd, parent, parent.offset);
+    } else {
+        if (node_to_remove.next != 0) {
+            auto next = read_node_header_from_db(fd, node_to_remove.next);
+            next.prev = node_to_remove.prev;
+            write_node_header_to_db(fd, next, next.offset);
+        }
+        if (node_to_remove.prev != 0) {
+            auto prev = read_node_header_from_db(fd, node_to_remove.prev);
+            prev.next = node_to_remove.next;
+            write_node_header_to_db(fd, prev, prev.offset);
+        }
+    }
+
+    // рекурсивно удаляем все дочерние ноды удаляемой ноды
     while (!rm_nodes_offset.empty()) {
         auto node_offset = rm_nodes_offset.top();
         rm_nodes_offset.pop();
         node_to_remove = read_node_header_from_db(fd, node_offset);
-        remove_node_from_index(node_to_remove.id, node_to_remove.parent_id);
+        remove_node_from_index(node_to_remove.id);
         remove_node_from_db(fd, node_to_remove);
 
         if (node_to_remove.first_child != 0) {
@@ -130,7 +127,7 @@ bool delete_node(int32_t fd, int64_t id) {
         }
         while (node_to_remove.next != 0) {
             node_to_remove = read_node_header_from_db(fd, node_to_remove.next);
-            remove_node_from_index(node_to_remove.id, node_to_remove.parent_id);
+            remove_node_from_index(node_to_remove.id);
             remove_node_from_db(fd, node_to_remove);
             if (node_to_remove.first_child != 0) {
                 rm_nodes_offset.push(node_to_remove.first_child);
@@ -141,13 +138,13 @@ bool delete_node(int32_t fd, int64_t id) {
     return true;
 }
 
-bool update_node(int32_t fd, int64_t id, const std::unordered_map<std::string, std::string>& node_data) {
+bool update_node(int32_t fd, int32_t id, const std::unordered_map<std::string, std::string> &node_data) {
     if (id == 0) { // корневой узел нельзя удалять
         printf("Cannot update a node with ID 0.");
         return false;
     }
     if (idx.id_to_offset.count(id) == 0) {
-        printf("Node with such an id{%ld} doesn't exist!\n", id);
+        printf("Node with such an id{%d} doesn't exist!\n", id);
         return false;
     }
     if (!validate_node_by_schema(fd, node_data)) {
@@ -163,8 +160,8 @@ bool update_node(int32_t fd, int64_t id, const std::unordered_map<std::string, s
     auto updated_offset = write_node_to_db(fd, node_to_update);
 
     // нужно обновить ссылки у родителя и соседних детей на нас
-    auto parent = read_node_header_from_db(fd, idx.id_to_offset[node_to_update.parent_id]);
-    if (idx.parent_to_childs[parent.id].size() == 1) { // если мы единственный ребенок (у родителя есть на нас указатель
+    auto parent = read_node_header_from_db(fd, node_to_update.parent);
+    if (parent.first_child == node_to_update.offset) { // если мы единственный ребенок (у родителя есть на нас указатель
         parent.first_child = updated_offset;
         write_node_header_to_db(fd, parent, parent.offset);
     } else {
--- commands/commands.h
+++ commands/commands.h
@@ -3,33 +3,59 @@
 #include <cinttypes>
 #include <unordered_map>
 #include <string>
-#include <list>
+#include <utility>
+#include <vector>
 
 enum command_type {
     ADD,
     UPDATE,
-    FIND_BY_ID,
-    FIND_BY_PARENT,
-    FIND_ALL,
-    DELETE,
+    FIND,
+    DELETE
 };
+
+enum comparison_operator {
+    NOP,
+    EQUAL,
+    NOT_EQUAL
+};
+
+const std::string comparison_operator_strings[] = {
+        "NOP",
+        "EQUAL",
+        "NOT_EQUAL"
+};
+
+struct condition {
+    std::string name_value;
+    std::string value;
+    comparison_operator operation;
+
+    condition() {}
+
+    condition(std::string name_of_value, std::string value, comparison_operator operation) {
+        this->name_value = std::move(name_of_value);
+        this->value = std::move(value);
+        this->operation = operation;
+    }
+
+    std::string to_string() const {
+        return name_value + ' ' + comparison_operator_strings[operation] + ' ' + value;
+    }
+};
+
 // TODO добавить описания
 // TODO подумать над структурой запроса
-struct command { // TODO query должен строить пользователь который пользуется модулем
-
+struct query { // TODO query должен строить пользователь который пользуется модулем
+    command_type command;
+    std::vector<struct condition> conditions;
 };
 
-int64_t add_node(int32_t fd, int64_t parent_id, const std::unordered_map<std::string, std::string> &node_data);
+int64_t add_node(int32_t fd, int32_t parent_id, const std::unordered_map<std::string, std::string> &node_data);
 
-///{{{абстрагировать в итератор над элементами
-std::unordered_map<std::string, std::string> find_node_by_id(int32_t fd, int64_t id);
-std::list<std::unordered_map<std::string, std::string>> find_node_by_parent(int32_t fd, int64_t parent_id);
-std::list<std::unordered_map<std::string, std::string>> find_all(int32_t fd);
-std::list<std::unordered_map<std::string, std::string>>
-find_node_by_condition(int32_t fd, const std::pair<std::string, std::string>& condition);
-///}}}
-
-bool update_node(int32_t fd, int64_t id, const std::unordered_map<std::string, std::string>& node_data);
-bool delete_node(int32_t fd, int64_t id);
+std::unordered_map<std::string, std::string> find_node_by_id(int32_t fd, int32_t id);
+struct result_iterator find(int32_t fd, const struct condition &condition);
+struct result_iterator find_all(int32_t fd);
+bool update_node(int32_t fd, int32_t id, const std::unordered_map<std::string, std::string>& node_data);
+bool delete_node(int32_t fd, int32_t id);
 
 #endif //LLP_DATABASE_COMMANDS_H
